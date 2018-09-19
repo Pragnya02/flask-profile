5466e9e10df2458c491a707eaa4fef89be2ad765
Pragnya Srinivasan <psrinivasan@Pragnyas-MBP.fios-router.home>
2018-09-19 00:03:14 -0400

    Adding application.py

--- application.py
+++ application.py
@@ -4,10 +4,10 @@ from flask import Flask, render_template
 # create the application object
 application = Flask(__name__)
 application.debug=True
-application.secret_key = 'AKIASKWOKSTJBTF526PE'
+#application.secret_key = 'AKIASKWOKSTJBTF526PE'
 # use decorators to link the function to a url
 @application.route('/')
-@application.route('/index')
+@application.route('/index',methods=[‘GET’])
 def home():
     return render_template('welcome.html')
 
@@ -15,4 +15,4 @@ def home():
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+    application.run()
